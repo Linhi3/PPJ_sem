ecd0ae91b68986699f4024a0f16805fd424704ad
Linhi3 <skvortom@gmail.com>
2024-05-09 11:06:43 +0200

    Project start

---
+++ src/main/java/org/linhart/ppj/sem/PpjSemApplication.java
@@ -0,0 +1,13 @@
+package org.linhart.ppj.sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpjSemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpjSemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/linhart/ppj/sem/PpjSemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.linhart.ppj.sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpjSemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
